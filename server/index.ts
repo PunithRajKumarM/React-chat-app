a425d03925c8c3de47f8b18ab9236aaef37c851f
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-08 11:48:09 +0530

    installed axios and body parser

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import { AppDataSource } from "./src/data-source";
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 4000;
+const router = express.Router();
+const REDIRECT_URI = "http://localhost:3000/auth/google/callback";
+
 require("dotenv").config();
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 (async () => {
   try {
@@ -17,5 +22,3 @@ app.use(express.json());
     console.log("Failed to connect the database!!!", error);
   }
 })();
-
-
