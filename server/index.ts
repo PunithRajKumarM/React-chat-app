08adf61d6cbfa4df09a5cbb02cd4032c1d78358a
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-08 10:35:23 +0530

    installed dot env and initialized data source

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,21 @@
 import { AppDataSource } from "./src/data-source";
+const express = require("express");
+const app = express();
+const PORT = 4000;
+require("dotenv").config();
 
-AppDataSource.initialize()
-  .then(async () => {})
-  .catch((error) => console.log(error));
+app.use(express.json());
+
+(async () => {
+  try {
+    await AppDataSource.initialize();
+    console.log("Database connected!!!");
+    app.listen(PORT, () => {
+      console.log("Server is listening on port ", PORT);
+    });
+  } catch (error) {
+    console.log("Failed to connect the database!!!", error);
+  }
+})();
+
+
