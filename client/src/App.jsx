df0604c04181684aaa26637010d1ab287ff3e365
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-11 19:35:23 +0530

    modified the env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <button>Click</button>
+    </>
+  );
 }
--- server/src/controllers/controllers.ts
+++ server/src/controllers/controllers.ts
@@ -1,9 +1,31 @@
 require("dotenv").config();
+import { OAuth2Client } from "google-auth-library";
 import { AppDataSource } from "../data-source";
 import { Message } from "../entity/Message";
 import { Request, Response } from "express";
 
 const messageRepo = AppDataSource.getRepository(Message);
+
+export const googleAuth = async (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Referrer-Policy", "no-referrer-when-downgrade");
+
+  const redirectUrl = "http://localhost:3000";
+
+  const oAuth2Client = new OAuth2Client(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    redirectUrl
+  );
+
+  const authorizeUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: "https://www.googleapis.com/auth/userinfo.profile openid",
+    prompt: "consent",
+  });
+};
+
+// get User
 
 export const sendMessage = async (req: Request, res: Response) => {
   try {
