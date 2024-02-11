88023ff6d98cb0cb84546ff74bb6cb81022af8b4
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-11 16:10:24 +0530

    installed react-google-login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,4 @@
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 export default function App() {
-  return (
-    <>
-      <GoogleOAuthProvider
-        clientId={ProcessingInstruction.env.REACT_APP_CLIENT_ID}
-      >
-        App
-      </GoogleOAuthProvider>
-    </>
-  );
+  return <>App</>;
 }
