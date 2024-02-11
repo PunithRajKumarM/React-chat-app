f825059003340a2dcfa9291556a1f0a191422e45
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-11 17:50:08 +0530

    deleted login, logout and google auth file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import GoogleAuth from "./components/GoogleAuth/GoogleAuth";
+
 export default function App() {
-  return (
-    <>
-      <GoogleAuth />
-    </>
-  );
+  return <></>;
 }
