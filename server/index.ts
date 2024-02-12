df282e26f434dea947af3758ef6d77e520e62450
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-12 10:50:28 +0530

    initialized socket.io in server and started server using http

--- server/index.ts
+++ server/index.ts
@@ -8,25 +8,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
 const PORT = 4000;
-
 require("dotenv").config();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET,POST,PUT,PATCH,DELETE"],
-    credentials: true,
-  })
-);
+
+app.use(cors());
+const server = http.createServer(app);
 app.use(express.json());
 app.use(bodyParser.json());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected ${socket.id}`);
+  });
+});
 
 (async () => {
   try {
     await AppDataSource.initialize();
     console.log("Database connected!!!");
-    app.listen(PORT, () => {
-      console.log("Server is listening on port ", PORT);
+    server.listen(PORT, () => {
+      console.log("SERVER RUNNING IN THE PORT", PORT);
     });
   } catch (error) {
     console.log("Failed to connect the database!!!", error);
