073a174c140f4642ca765aad556879da06c73bad
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-12 10:27:55 +0530

    installed socket.io in client

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import classes from "./Chat.module.css";
-
 export default function Chat() {
   return (
     <>
       <div className={classes.chatsPage}>
         <div className={classes.chatBox}>
-          <div className={classes.chats}></div>
+          <div className={classes.chats}>hello</div>
           <div className={classes.sendChat}>
             <input type="text" />
             <button>Send</button>
