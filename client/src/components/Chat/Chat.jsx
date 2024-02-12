845d720e24318aac1be5d52855c8f50117fc7adc
PunithRajKumarM <punithrajkumar.m@celestialsys.com>
2024-02-12 09:07:39 +0530

    created chat content box

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -4,7 +4,15 @@ import classes from "./Chat.module.css";
 export default function Chat() {
   return (
     <>
-      <div className={classes.chatsPage}></div>
+      <div className={classes.chatsPage}>
+        <div className={classes.chatBox}>
+          <div className={classes.chats}></div>
+          <div className={classes.sendChat}>
+            <input type="text" />
+            <button>Send</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
